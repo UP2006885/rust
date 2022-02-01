3f754ba67beb6e361ffa73371169c77d74a43ffb
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-02-01 01:18:46 +0000

    Basic output macro formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!"); // Macro
+    println!("Number: {}", 25); // Single Placeholder
+    println!("My name is {} I am {} years old.", "Harry", 20); // Multiple Placeholders
+    println!("My name is {0}. I am {1} years old. {0} is my name.", "Harry", 20); // Positional Arguments
+    println!("My name is {name}. I am {age} years old.", name = "Harry", age = 20); // Named Arguments
+
+    // {:b},{:x},{:o}
+    println!("The number {0} is {0:b} in binary.", 67);
+    println!("The number {0} is {0:x} in hexadecimal.", 67);
+    println!("The number {0} is {0:o} in octal.", 67);
+    // ----------------------------------------------------------------
+    println!("Number : {0} \nBinary:{0:b} Hexadecimal:{0:x} Octal:{0:o}", 97);
+
+    println!("{0} + {0} = {1}",10, 10 + 10); // Basic math.
+
+    println!("This is a multiple value placeholder {:?}", ("Hello there", 92, "Bye")); // Placeholder for a Debug Trait
 }
